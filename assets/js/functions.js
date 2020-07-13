3b841bc9cc2c13e5861849999f66e17d22535123
benjanknoetze <benjanknoetze@gmail.com>
2020-07-13 14:40:22 +0200

    Personal Website
    Benjan's short bio with links to contact details and CV in pdf format on Google Drive.

---
+++ assets/js/functions.js
@@ -0,0 +1,25 @@
+// Hover effects for Cards - Index Page //
+
+// People //
+$(card1).mouseover(function () {
+  $(h5One).css("font-size", "20.7px", "transition", "ease-in-out 0.3s");
+});
+$(card1).mouseout(function () {
+  $(h5One).css("font-size", "20px", "transition", "ease-in-out 0.3s");
+});
+
+// Design //
+$(card2).mouseover(function () {
+  $(h5Two).css("font-size", "20.7px", "transition", "ease-in-out 0.3s");
+});
+$(card2).mouseout(function () {
+  $(h5Two).css("font-size", "20px", "transition", "ease-in-out 0.3s");
+});
+
+// Design //
+$(card3).mouseover(function () {
+  $(h5Three).css("font-size", "20.7px", "transition", "ease-in-out 0.3s");
+});
+$(card3).mouseout(function () {
+  $(h5Three).css("font-size", "20px", "transition", "ease-in-out 0.3s");
+});
