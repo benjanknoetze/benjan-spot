fb16921b0b9d68c44bb4b589d6680c4445ce78d6
benjanknoetze <56556262+benjanknoetze@users.noreply.github.com>
2020-10-30 11:31:32 +0200

    google analytics

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -2,10 +2,9 @@
 
 /*You can use this built-in component to append elements to the <head> element of the page. More info here: https://github.com/zeit/next.js/#populating-head 
 Notice that we add the link for the stylesheet for using React-Bootstrap components to the <Head>. */
-import Head from 'next/head'
 import Header from './Header' //Header component
 
-{/* Global layout */}
+{/* Global layout */ }
 const Layout = props => (
 	<div>
 		<Head>
@@ -17,13 +16,28 @@ const Layout = props => (
 				crossOrigin="anonymous"
 			/>
 			{/* Google Fonts */}
-			<link 
-			rel="stylesheet" 
-			href="https://fonts.googleapis.com/css?family=Allerta" 
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css?family=Allerta"
 			/>
-			<link 
-			rel="stylesheet" 
-			href="https://fonts.googleapis.com/css?family=Bitter:400,700" 
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css?family=Bitter:400,700"
+			/>
+			{/* Google Analytics */}
+			<script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-63HLEECX4G"
+			/>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-63HLEECX4G');
+        `,
+				}}
 			/>
 		</Head>
 		<div>
