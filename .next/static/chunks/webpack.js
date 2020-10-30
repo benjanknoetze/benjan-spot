8f3c26bd6aeeebcb83014728ea9f08c912b3c24b
benjanknoetze <56556262+benjanknoetze@users.noreply.github.com>
2020-10-30 15:56:55 +0200

    fix formspree

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5dd2b4b233cc1760c3d0";
+/******/ 	var hotCurrentHash = "032837809f196b279062";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,21 +1,87 @@
 //Contact page form
-
+import React from "react";
 import { Button } from 'react-bootstrap'
-import { FormspreeProvider } from '@formspree/react';
+import { inherits } from "util";
 
 export default class MyForm extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.submitForm = this.submitForm.bind(this);
-	  this.state = {
-		status: ""
-	  };
-	}
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  render() {
+    const { status } = this.state;
+    return (
+		<div>
+	  <h5 className="Heading pl-3 mt-3">Get in touch</h5>
+      <form className="p-3"
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/xrgoqvaq"
+        method="POST"
+      >
+        <label className="formLabel">Email:</label>
+		  <br></br>
+		<input className="formInput" type="email" name="email" /> <br></br>
+        <label className="formLabel mt-2">Message:</label> <br></br>
+		<input className="formInput pb-5" type="text" name="message" /> <br></br>
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button style={{borderRadius: 4, border: "none", backgroundColor: "#635598", fontFamily: "Bitter, serif", color: "white", marginTop: 15, paddingTop: 6, paddingBottom: 6, paddingLeft: 10, paddingRight: 10}}>Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
+	  <style global jsx>{`
+			.Heading {
+				color: #0d0411;
+				font-family: Allerta, sans-serif;
+				font-weight: 800;
+			}
+			.contactText {
+				color: #0d0411;
+				font-family: Bitter, serif;
+			}
+			.formLabel {
+				font-family: Bitter, serif;
+			}
+			.formInput {
+				background-color: #cdc9de;
+				width: 75%;
+				border: none;
+				border-radius: 4px;
+			}
+			.submitButton {
+				width: 110px;
+			}
+			`}</style>
+	  </div>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
+   
   
-	render() {
-	  const { status } = this.state;
+	/* render() {
+	    const { status } = this.state;
 	  return (
-		<FormspreeProvider project="{contact}">
+		<div>
 		<h5 className="Heading pl-3 mt-3">Get in touch</h5>
 		<form 
 		  className="p-3"
@@ -54,26 +120,11 @@ export default class MyForm extends React.Component {
 				width: 110px;
 			}
 			`}</style>
-		</FormspreeProvider>
+		</div>
 	  );
-	}
+	} */
+
+
+
   
-	submitForm(ev) {
-	  ev.preventDefault();
-	  const form = ev.target;
-	  const data = new FormData(form);
-	  const xhr = new XMLHttpRequest();
-	  xhr.open(form.method, form.action);
-	  xhr.setRequestHeader("Accept", "application/json");
-	  xhr.onreadystatechange = () => {
-		if (xhr.readyState !== XMLHttpRequest.DONE) return;
-		if (xhr.status === 200) {
-		  form.reset();
-		  this.setState({ status: "SUCCESS" });
-		} else {
-		  this.setState({ status: "ERROR" });
-		}
-	  };
-	  xhr.send(data);
-	}
-  }
+
