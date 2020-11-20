21c8065b6aa685d45a279b907dbc40801020fa51
benjanknoetze <56556262+benjanknoetze@users.noreply.github.com>
2020-11-20 11:06:22 +0200

    preload bootstrap css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c8f1886e0e75bf40c470";
+/******/ 	var hotCurrentHash = "f8a67dd89c2851bb279b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -12,10 +12,11 @@ const Layout = props => (
 		<Head>
 			{/* Bootstrap */}
 			<link
-				rel="stylesheet"
+				rel="preload"
 				href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 				crossOrigin="anonymous"
+				as="style"
 			/>
 			{/* Google Fonts */}
 			<link
